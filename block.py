26f34f76528cb953a02001412aa0c35a041ffc1d
Kaito <mail.kaito03@gmail.com>
2019-09-07 12:19:39 +0900

    implement Block's constructor

---
+++ block.py
@@ -0,0 +1,27 @@
+import json
+from time import time
+from hashlib import sha256
+
+class Block:
+    def __init__(self, transactions: list, previous_hash: str) -> None:
+        self.time = time()
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.set_hash()
+
+    def set_hash(self) -> None:
+        self.hash = self.generate_hash()
+
+    def generate_hash(self) -> str:
+        block_hash = sha256(self.block_header.encode())
+        return block_hash.hexdigest()
+
+    @property
+    def block_header(self) -> str:
+        return json.dumps({
+            'time_stamp': self.time,
+            'transactions': self.transactions,
+            'previous_hash': self.previous_hash,
+            'nonce': self.nonce,
+        }, sort_keys=True)

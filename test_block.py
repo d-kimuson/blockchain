c4317f2fb511251e159aef16e010cf62182eb3a1
Kaito <mail.kaito03@gmail.com>
2019-09-07 12:48:21 +0900

    move

---
+++ test_block.py
@@ -0,0 +1,17 @@
+import pytest
+from block import Block
+
+@pytest.fixture
+def block():
+    return Block([], "0")
+
+
+def test_generate_hash(block):
+    res = block.generate_hash()
+    assert type(res) is str
+
+def test_set_hash(block):
+    block.set_hash()
+
+def test_block_header(block):
+    assert type(block.block_header) is str
